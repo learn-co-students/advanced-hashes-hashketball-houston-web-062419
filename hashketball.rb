20404f453d568caba377922d9a208d28e148ab96
Aaron B <abriscoe2008@gmail.com>
2019-06-09 00:17:01 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,36 @@
-# Write your code here!
+def game_hash
+  {
+    :home => {
+      :team_name => "Team Name", 
+      :colors => ["Color1", "Color2"],
+      :players => [{
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      }]
+    }
+    :away => {
+      :team_name => "Team Name", 
+      :colors => ["Color1", "Color2"],
+      :players => [{
+        :number => "",
+        :shoe => "",
+        :points => "",
+        :rebounds => "",
+        :assists => "",
+        :steals => "",
+        :blocks => "",
+        :slam_dunks => ""
+      }]
+    }
+    
+end 
+    
 
 
 
